f64604d20530d7e9a1e0a5fb90d6e887e5fdf212
JeremyLeigh <le_jeremy@live.ca>
2021-01-04 23:55:22 -0500

    More skelleton build
    Removed hrs, built out more of homepage, increased responsivity

--- js/main.js
+++ js/main.js
@@ -5,22 +5,30 @@ var barOne = document.querySelector("#hamOne");
 var barTwo = document.querySelector("#hamTwo");
 var barThree = document.querySelector("#hamThree");
 var navPage = document.querySelector("#hamSplash");
+var splashFog = document.querySelector('#fog');
 var exitSet = 0;
+
 
 //FUNCT
 function  triggerHamburger()
 {
   if (exitSet == 0){
     navPage.style.width = 100+"%";
+    splashFog.style.width = 100+"vw";
     barTwo.style.transform = "translateY("+20+"px)";
-    barOne.style.transform = "rotate("+45+"deg) translate("+7+"px,"+7+"px)";
-    barThree.style.transform = "rotate(-"+45+"deg) translate("+7+"px,-"+7+"px)";
+    barOne.style.transform = "rotate("+45+"deg) translate("+6+"px,"+6+"px)";
+    barThree.style.transform = "rotate(-"+45+"deg) translate("+5+"px,-"+5+"px)";
+    barTwo.style.backgroundColor = "#d21404";
+    barTwo.style.height = 2+"px";
     console.log("Trigger Burger");
     exitSet = 1;
   }
   else {
     navPage.style.width = 0+"px";
+    splashFog.style.width = 0+"px";
     barTwo.style.transform = "translateX("+0+"px)";
+    barTwo.style.backgroundColor = "#000000";
+    barTwo.style.height = 4+"px";
     barOne.style.transform = "rotate(0deg)";
     barThree.style.transform = "rotate(0deg)";
     console.log("Burger Reset");
